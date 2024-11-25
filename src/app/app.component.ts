96936ff4c438f48fec856b85f4ed55497349166e
Abhishek Pardhiya <abhishekpardhiya@gmail.com>
2024-11-25 23:45:28 +0530

    new slider and service design added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,7 @@ import { HomeComponent } from "./pages/home/home.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent],
+  imports: [HeaderComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pages/img-slider/img-slider.component.ts
@@ -0,0 +1,53 @@
+import { CommonModule, isPlatformBrowser } from '@angular/common';
+import { Component, Inject, PLATFORM_ID } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
+declare const Swiper: any;
+
+@Component({
+  selector: 'app-img-slider',
+  standalone: true,
+  imports: [CommonModule,MatButtonModule,MatIconModule],
+  templateUrl: './img-slider.component.html',
+  styleUrl: './img-slider.component.css',
+})
+export class ImgSliderComponent {
+  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit() {
+    if (isPlatformBrowser(this.platformId)) {
+      new Swiper('.slider-wrapper', {
+        loop: true,
+        grabCursor: true,
+        spaceBetween:10,
+        autoplay: {
+          delay: 2000,
+        },
+        pagination: {
+          el: '.swiper-pagination',
+          clickable: true,
+          dynamicBullets: true
+        },
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        breakpoints: {
+          412: {
+            slidesPerView: 1,
+          },
+          768: {
+            slidesPerView: 2,
+          },
+          1024: {
+            slidesPerView: 3,
+          },
+          1440: {
+            slidesPerView: 4,
+          },
+        },
+      });
+    }
+  }
+}
