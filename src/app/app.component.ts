603c2ced228cff6d5e7e73c36a20c317c100894c
Abhishek Pardhiya <abhishekpardhiya@gmail.com>
2024-11-26 15:14:32 +0530

    top contact bar added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,14 +3,25 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./pages/header/header.component";
 import { FooterComponent } from "./pages/footer/footer.component";
 import { HomeComponent } from "./pages/home/home.component";
+import { MatIconModule } from '@angular/material/icon';
+import { faYoutube,faFacebook,faInstagram,faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, HomeComponent],
+  imports: [HeaderComponent, HomeComponent,MatIconModule,FontAwesomeModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Tadoba-Solutions';
+
+  instagram = faInstagram;
+  facebook = faFacebook;
+  youtube = faYoutube;
+  twitter = faXTwitter;
+
+
+
 }
