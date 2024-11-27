2803cc3fa3cc8038b9db93990f639aacd63a6d4e
Abhishek Pardhiya <abhishekpardhiya@gmail.com>
2024-11-28 01:34:23 +0530

    our-products & product components added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,15 @@
 import { Routes } from '@angular/router';
 import { CareerComponent } from './pages/career/career.component';
 import { HomeComponent } from './pages/home/home.component';
+import { OurProductsComponent } from './pages/our-products/our-products.component';
+import { ViewProductComponent } from './pages/view-product/view-product.component';
 
 export const routes: Routes = [
     {path: '',component:HomeComponent},
     {path:'career',component:CareerComponent},
+    {
+        path:'our-products',
+        component:OurProductsComponent,
+    },
+    {path:'product',component:ViewProductComponent}
 ];
---
+++ src/app/pages/our-products/our-products.component.ts
@@ -0,0 +1,61 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { ProductCardComponent } from "../../components/product-card/product-card.component";
+import { Product } from '../../models/Product';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-our-products',
+  standalone: true,
+  imports: [CommonModule, ProductCardComponent,MatIconModule],
+  templateUrl: './our-products.component.html',
+  styleUrl: './our-products.component.css'
+})
+export class OurProductsComponent {
+
+  products:Product[]=[
+    {
+      productName:"Auto ON/OFF 1/3 Phase With Dry Run Protection (Universal)",
+      productRating:4.5,
+      productAvailability:"in-stock",
+      productPrice:2359.00,
+      productImg:"./assets/images/product/own-product/6.png" 
+   },
+   {
+    productName:"Auto OFF Single Phase With (Universal)",
+    productRating:3.5,
+    productAvailability:"in-stock",
+    productPrice:759.00,
+    productImg:"./assets/images/product/own-product/1.png" 
+  },
+  {
+    productName:"Auto OFF Single Phase (Tullu)",
+    productRating:4.5,
+    productAvailability:"in-stock",
+    productPrice:659.00,
+    productImg:"./assets/images/product/own-product/3.png" 
+  },
+  {
+    productName:"Auto OFF Single Phase (Submersible)",
+    productRating:4.5,
+    productAvailability:"in-stock",
+    productPrice:559.00,
+    productImg:"./assets/images/product/own-product/3.png" 
+  },
+  {
+    productName:"AC/DC Switch",
+    productRating:4.5,
+    productAvailability:"in-stock",
+    productPrice:559.00,
+    productImg:"./assets/images/product/own-product/ac-dc-switch.jpg" 
+  },
+  {
+    productName:"Portable Soil Sensor Device",
+    productRating:2.5,
+    productAvailability:"in-stock",
+    productPrice:1799.00,
+    productImg:"./assets/images/product/own-product/portable-soil-sensor-device.jpg" 
+  }
+  ]
+
+}
