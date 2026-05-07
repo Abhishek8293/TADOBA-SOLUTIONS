c72189a116ef061507bec7bcae0036d0ca2ad585
Abhishek Pardhiya <abhishekpardhiya@gmail.com>
2026-05-07 18:03:37 +0530

    summer vt section added

---
+++ src/app/components/vocational-training/vocational-training.component.ts
@@ -0,0 +1,14 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+
+@Component({
+  selector: 'app-vocational-training',
+  standalone: true,
+  imports: [CommonModule, MatIconModule],
+  templateUrl: './vocational-training.component.html',
+  styleUrl: './vocational-training.component.css'
+})
+export class VocationalTrainingComponent {
+
+}
